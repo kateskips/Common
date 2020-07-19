e4eacbe26ae81e2b14c3e79ec2346283011c271e
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-07-19 12:28:26 -0400

    updated themes controller with methods

--- app/controllers/api/v1/themes_controller.rb
+++ app/controllers/api/v1/themes_controller.rb
@@ -1,6 +1,41 @@
 module Api
     module V1
         class ThemesController < ApplicationController
+            def index
+                themes = Themes.all
+                render json: ThemeSerializer.new(themes).serialized_json
+             end
+
+            def show
+              theme = Theme.find_by(id: params[:id])
+                render json: ThemeSerializer.new(theme).serialized_json
+            end
+
+            def create
+              theme = Theme.new(theme_params)
+
+              if theme.save  
+                render json: ThemeSerializer.new(theme).serialized_json
+              else
+                render json: { error: theme.errors.messages}, status: 422
+              end
+            end
+
+            def update
+              theme = Theme.find_by(id: params[:id])
+
+               if theme.update(theme_params)  
+                render json: ThemeSerializer.new(theme).serialized_json
+              else
+                render json: { error: theme.errors.messages}, status: 422
+              end
+            end
+
+            private
+
+            def user_params
+              params.require(:theme).permit(:topic)
+            end
         end
     end
 end
