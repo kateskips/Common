4b434cbfe8091e600ea0c3bfc118387791001624
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-07-27 15:38:41 -0400

    theme display card

--- app/javascript/packs/components/Themes/Theme.js
+++ app/javascript/packs/components/Themes/Theme.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Card = styled.div`
+border: 1.5px solid #000000;
+background: #fff
+`
+const TopicLink = styled.div`
+padding: 10px 0 10px 0;
+h4 {
+    font-size: 16px;
+} 
+`
 
 const Theme = (props) => {
     return (
-        <div className="card">
-            <div className="theme-questions">{props.attributes.topic}</div>
-        </div>
+        <Card>
+            <TopicLink>
+                <Link to={(`/themes/${props.id}`)}>{props.attributes.topic}</Link>
+            </TopicLink>
+        </Card>
     )
 }
 
