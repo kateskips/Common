fb8d026fa60e26421b09d300fb9c94cb48d27106
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-07-31 11:25:33 -0400

    created an about page and added content

---
+++ app/javascript/packs/components/About.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = styled.div`
+text-align: center;
+max-width: 1200px;
+margin-left: auto;
+margin-right: auto;
+`
+const Header = styled.div`
+padding: 100px 100px 10px 100px;
+
+h1 {
+   font-size: 85px;
+}
+`
+const Subheader = styled.div`
+font-weight: 300;
+font-size: 10px;
+`
+
+function About() {
+    return (
+        <Home>
+            <Header>
+                <h1>Common</h1>
+                <Subheader>
+                    <h4>Thoughtful ways to bond with a fellow human.</h4>
+                </Subheader>
+            </Header>
+            <div>
+                <center>
+                    <h1>About</h1>
+                    <p>blah blah blah blah blah blah blah</p>
+                </center>
+            </div>
+        </Home>
+    )
+}
+
+export default About
