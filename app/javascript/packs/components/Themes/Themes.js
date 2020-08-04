7d3fbd281a29265ee61df39ab9a9d7106e56bca7
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-08-04 11:24:18 -0400

    created button links to about and create pages

--- app/javascript/packs/components/Themes/Themes.js
+++ app/javascript/packs/components/Themes/Themes.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 import Theme from './Theme'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Home = styled.div`
 text-align: center;
@@ -27,6 +28,34 @@ grid-template-columns: repeat(3, 1fr);
 grid-gap: 20px;
 width: 100%;
 padding: 20px;
+`
+const AboutButton = styled.button`
+border: 2px solid black;
+background-color: #FA9D00;
+max-width: 100px;
+margin-left: auto;
+margin-right: auto;
+color: black;
+padding: 10px 15px;
+border-radius: 3px;
+`
+
+const CreateButton = styled.button`
+border: 2px solid black;
+background-color: #ED0026;
+max-width: 100px;
+margin-left: auto;
+margin-right: auto;
+color: black;
+padding: 10px 15px;
+border-radius: 3px;
+`
+
+const Buttons = styled.div`
+margin: 0 auto;
+max-width: 175px;
+display: flex;
+justify-content: space-between;
 `
 
 const Themes = () => {
@@ -64,6 +93,13 @@ const Themes = () => {
             </Grid>
             </Header>
             <img src="https://64.media.tumblr.com/7973e37fcdd513932e199fada987ad48/tumblr_mqh3isKTJX1rs2sbno1_500.gif"></img>
+            <Buttons className="buttons"><Link to='/about'>
+                <AboutButton type="button" className="About-button">About</AboutButton>
+            </Link>
+                <Link to='/create'>
+                    <CreateButton type="button" className="Create-button">Create</CreateButton>
+                </Link>
+            </Buttons>
         </Home>
     )
 }
