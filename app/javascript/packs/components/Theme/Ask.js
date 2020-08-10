f29c37e433f99780a8edfce8a31260622f43c73d
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-08-10 13:12:40 -0400

    created an ask card props

---
+++ app/javascript/packs/components/Theme/Ask.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Card = styled.div`
+border: 1.5px solid #000000;
+background: #fff
+`
+
+const Ask = (props) => {
+    return (
+        <Card>
+            <h4>{props.question}</h4>
+        </Card>
+    )
+}
+
+export default Ask;
