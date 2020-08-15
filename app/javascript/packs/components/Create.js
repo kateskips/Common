41cb0b17a9736de7386e3792f95fc794bd846811
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-08-14 22:11:02 -0400

    added backbtn with link

--- app/javascript/packs/components/Create.js
+++ app/javascript/packs/components/Create.js
@@ -2,12 +2,35 @@ import React from 'react';
 import HomeWrapper from './HomeWrapper'
 import AskForm from './AskForm'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const BackBtn = styled.button`
+border: 2px solid black;
+background-color: #eef5b3;
+max-width: 100px;
+margin-left: auto;
+margin-right: auto;
+color: black;
+padding: 10px 10px;
+border-radius: 3px;
+`
+
 
 const Create = () => {
     return (
         <div>
             <HomeWrapper />
+            <br></br>
+            <center>
+                <h1><u>Create a Question</u></h1>
+            </center>
             <AskForm />
+            <br></br>
+            <center>
+            <Link to='/'>
+                <BackBtn type="button" className="Homepage-button">Back</BackBtn>
+                </Link>
+            </center>
         </div>
     )
 }
