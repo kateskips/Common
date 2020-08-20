207150352f69525c745621e1deeb39d2760dde78
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-08-20 14:19:16 -0400

    created a random reducer

---
+++ app/javascript/reducers/random.js
@@ -0,0 +1,10 @@
+const randomReducer = (state = null, action) => {
+    switch (action.type) {
+        case "RANDOMIZER":
+            return state
+        default:
+            return state
+    }
+}
+
+export default randomReducer
