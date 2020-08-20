569855301045ba1940812a05573d396126131b41
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-08-20 14:22:29 -0400

    import createStore and allReducers

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,13 +1,20 @@
 // Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
-//import Home from './components/Home/Home'
 import App from './components/App'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import { createStore } from 'redux'
+import allReducers from '../reducers'
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
