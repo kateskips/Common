5ac0e06305b3f209e6330df70138662502695243
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-08-26 14:44:03 -0400

    created actions questions with functions

---
+++ app/javascript/packs/actions/questions.js
@@ -0,0 +1,35 @@
+export function asksHasErrored(bool) {
+    return {
+        type: 'ASKS_HAS_ERRORED',
+        hasErrored: bool
+    };
+}
+export function asksIsLoading(bool) {
+    return {
+        type: 'ASKS_IS_LOADING',
+        isLoading: bool
+    };
+}
+export function asksFetchDataSuccess(asks) {
+    return {
+        type: 'ASKS_FETCH_DATA_SUCCESS',
+        asks
+    };
+}
+
+export function asksFetchData(url) {
+    return (dispatch) => {
+        dispatch(asksIsLoading(true));
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                dispatch(asksIsLoading(false));
+                return response;
+            })
+            .then((response) => response.json())
+            .then((asks) => dispatch(asksFetchDataSuccess(asks)))
+            .catch(() => dispatch(asksHasErrored(true)));
+    };
+}
