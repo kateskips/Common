322ae3b16aa94e84b8c83ad73180e3f284146137
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-08-26 14:39:12 -0400

    created questions reducer with several functions

---
+++ app/javascript/packs/reducers/questions.js
@@ -0,0 +1,24 @@
+export function asksHasErrored(state = false, action) {
+    switch (action.type) {
+        case 'ASKS_HAS_ERRORED':
+            return action.hasErrored;
+        default:
+            return state;
+    }
+}
+export function asksIsLoading(state = false, action) {
+    switch (action.type) {
+        case 'ASKS_IS_LOADING':
+            return action.isLoading;
+        default:
+            return state;
+    }
+}
+export function asks(state = [], action) {
+    switch (action.type) {
+        case 'ASKS_FETCH_DATA_SUCCESS':
+            return action.asks;
+        default:
+            return state;
+    }
+}
