85790a94afd060386de2a6f09b08f1c734a7d0b3
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-08-27 08:48:21 -0400

    added semicolon

--- app/javascript/packs/components/Theme/Ask.js
+++ app/javascript/packs/components/Theme/Ask.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Card = styled.div`
 border: 1.5px solid #000000;
 padding: 1.5em 0;
-background: #fff
+background: #fff;
 `
 
 const Ask = (props) => {
