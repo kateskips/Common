7326f1c46ed297d8e7cb1a5b0385e708c10852e9
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-10-14 23:25:10 -0400

    Convert Ask to class with voting buttons

--- app/javascript/packs/components/Theme/Ask.js
+++ app/javascript/packs/components/Theme/Ask.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from "react"
 import styled from 'styled-components';
 
 const Card = styled.div`
@@ -7,12 +7,35 @@ const Card = styled.div`
 	background: #fff;
 `;
 
-const Ask = (props) => {
-	return (
-		<Card>
-			<h4>{props.question}</h4>
-		</Card>
-	);
-};
+class Ask extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			clicks: 0,
+			show: true
+		};
+	};
+	
+	voteUp = () => {
+		this.setState(prevState => ({ clicks: prevState.clicks + 1 }));
+	};
+	voteDown = () => {
+		this.setState(prevState => ({ clicks: prevState.clicks - 1 }));
+	};
+	ToggleClick = () => {
+		this.setState({ show: !this.state.show });
+	};
 
+	render() {
+		return (
+			<Card>
+				<h4>{this.props.question}</h4>
+				<button onClick={this.voteUp}>👍</button>
+				<button onClick={this.voteDown}>👎</button>
+				<button onClick={this.ToggleClick}>{this.state.show ? '🤭' : '😳'}</button>
+				{this.state.show ? <h2>{this.state.clicks}</h2> : ''}
+			</Card>
+		);
+	};
+};
 export default Ask;
