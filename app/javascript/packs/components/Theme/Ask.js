81a2253dc6ff8dee324a63de51882314195b80fe
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-10-28 12:16:37 -0400

    added comments

--- app/javascript/packs/components/Theme/Ask.js
+++ app/javascript/packs/components/Theme/Ask.js
@@ -7,15 +7,22 @@ const Card = styled.div`
 	background: #fff;
 `;
 
+
 class Ask extends Component {
+	//constructor method is used to initialize the state
 	constructor(props) {
 		super(props);
+		//want to use 'this.props' in the constructor 
 		this.state = {
+			//sets the initial state
 			clicks: 0,
 			show: true
 		};
 	};
 	
+	//implement methods: voteUp, voteDown, ToggleClick that the Ask component will run.
+	//this.setState schedules an update to the local component state, setState is asynchronous
+	//using prevState to override current state with the last state.
 	voteUp = () => {
 		this.setState(prevState => ({ clicks: prevState.clicks + 1 }));
 	};
@@ -24,9 +31,11 @@ class Ask extends Component {
 	};
 	ToggleClick = () => {
 		this.setState({ show: !this.state.show });
+		//adding ! operator as a boolean - 'negate'
 	};
 
 	render() {
+		//event handler --click, accessing them using 'this'
 		return (
 			<Card>
 				<h4>{this.props.question}</h4>
@@ -35,7 +44,7 @@ class Ask extends Component {
 				<button onClick={this.ToggleClick}>{this.state.show ? '🤭' : '😳'}</button>
 				{this.state.show ? <h2>{this.state.clicks}</h2> : ''}
 			</Card>
-		);
+		); 
 	};
 };
 export default Ask;
